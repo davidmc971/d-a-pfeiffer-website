741cf2fb0e6900b6b7512cd678cc2849d9cc95ec
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2021-09-25 20:28:07 +0200

    Preview page done

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import Badge from "./Badge";
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typography className="App-text" color={"white"}>Website coming soon!</Typography>
+        <Box height={100} />
+        <Badge />
       </header>
     </div>
   );
