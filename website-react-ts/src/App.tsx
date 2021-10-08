e14b76f1165e2deaaf16972c21f11dd7907fd465
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2021-10-08 11:57:17 +0200

    Navbar

--- website-react-ts/src/App.tsx
+++ website-react-ts/src/App.tsx
@@ -1,32 +1,19 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="dark" variant="dark" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">David Alexander Pfeiffer</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link href="#about">About Me</Nav.Link>
+              <Nav.Link href="#portfolio">Portfolio</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
