32038bfbc040c575c7864a104d18bccd04eb8496
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2021-10-12 23:16:19 +0200

    Update Intro.tsx

--- website-react-ts/src/Components/Intro.tsx
+++ website-react-ts/src/Components/Intro.tsx
@@ -12,7 +12,12 @@ import { Box } from "@mui/system";
 import React from "react";
 import profileImg from "../Assets/profile.jpg";
 import "./Intro.css";
-import { faLinkedin, faGithub, faGitlab, faDiscord } from "@fortawesome/free-brands-svg-icons";
+import {
+  faLinkedin,
+  faGithub,
+  faGitlab,
+  faDiscord,
+} from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 function Intro() {
@@ -30,35 +35,53 @@ function Intro() {
           <Container>
             <Stack spacing={2} alignItems="center">
               <Avatar src={profileImg} sx={{ width: 128, height: 128 }} />
-              <Typography variant="h3" textAlign="center">David Alexander Pfeiffer</Typography>
+              <Typography variant="h3" textAlign="center">
+                David Alexander Pfeiffer
+              </Typography>
               <Typography variant="h6" textAlign="center">
                 Freelance Software Developer and Computer Science Student
               </Typography>
               <ButtonGroup>
                 <Tooltip title="GitHub">
-                  <IconButton onClick={() => window.open('https://github.com/davidmc971')}>
+                  <IconButton
+                    onClick={() => window.open("https://github.com/davidmc971")}
+                  >
                     <FontAwesomeIcon icon={faGithub} />
                   </IconButton>
                 </Tooltip>
                 <Tooltip title="GitLab">
-                <IconButton onClick={() => window.open('https://gitlab.com/davidmc971')}>
-                  <FontAwesomeIcon icon={faGitlab} />
-                </IconButton>
+                  <IconButton
+                    onClick={() => window.open("https://gitlab.com/davidmc971")}
+                  >
+                    <FontAwesomeIcon icon={faGitlab} />
+                  </IconButton>
                 </Tooltip>
                 <Tooltip title="LinkedIn">
-                <IconButton onClick={() => window.open('https://linkedin.com/in/david-alexander-pfeiffer/')}>
-                  <FontAwesomeIcon icon={faLinkedin} />
-                </IconButton>
+                  <IconButton
+                    onClick={() =>
+                      window.open(
+                        "https://linkedin.com/in/david-alexander-pfeiffer/"
+                      )
+                    }
+                  >
+                    <FontAwesomeIcon icon={faLinkedin} />
+                  </IconButton>
                 </Tooltip>
                 <Tooltip title="Discord Server">
-                <IconButton onClick={() => window.open('https://discord.gg/RcTfXBkn2g')}>
-                  <FontAwesomeIcon icon={faDiscord} />
-                </IconButton>
+                  <IconButton
+                    onClick={() => window.open("https://discord.gg/RcTfXBkn2g")}
+                  >
+                    <FontAwesomeIcon icon={faDiscord} />
+                  </IconButton>
                 </Tooltip>
                 <Tooltip title="david.pfeiffer971@gmail.com">
-                <IconButton onClick={() => window.open('mailto:david.pfeiffer971@gmail.com')}>
-                  <FontAwesomeIcon icon={faEnvelope} />
-                </IconButton>
+                  <IconButton
+                    onClick={() =>
+                      window.open("mailto:david.pfeiffer971@gmail.com")
+                    }
+                  >
+                    <FontAwesomeIcon icon={faEnvelope} />
+                  </IconButton>
                 </Tooltip>
               </ButtonGroup>
             </Stack>
