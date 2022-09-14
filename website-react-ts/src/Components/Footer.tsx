232560620b50b0415ec0d7dd668107cff06968fa
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2022-09-14 19:38:08 +0200

    Imprint Dialog, no text yet

--- website-react-ts/src/Components/Footer.tsx
+++ website-react-ts/src/Components/Footer.tsx
@@ -1,7 +1,8 @@
-import { Grid, Link, Paper, Toolbar } from "@mui/material";
-import React from "react";
+import { Dialog, DialogActions, DialogContent, DialogTitle, Grid, Link, Paper, Toolbar, Typography } from "@mui/material";
+import React, { useState } from "react";
 
 function Footer() {
+  const [modalOpen, setModalOpen] = useState(false);
   return (
     <Paper sx={{ borderRadius: 0 }}>
       <Toolbar variant="dense">
@@ -15,7 +16,23 @@ function Footer() {
             Copyright 2021
           </Grid>
           <Grid item xs="auto">
-            <Link color="inherit" sx={{ cursor: "pointer" }}>Imprint (coming soon)</Link>
+            <Link
+              color="inherit"
+              sx={{ cursor: "pointer" }}
+              onClick={() => setModalOpen(!modalOpen)}
+            >
+              Imprint (coming soon)
+            </Link>
+            <Dialog
+              open={modalOpen}
+              onClose={() => setModalOpen(false)}
+              fullWidth={true}
+              maxWidth="md"
+              >
+              <DialogTitle>Imprint</DialogTitle>
+              <DialogContent></DialogContent>
+              <DialogActions></DialogActions>
+            </Dialog>
           </Grid>
         </Grid>
       </Toolbar>
