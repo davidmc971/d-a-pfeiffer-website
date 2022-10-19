48203daccfbde70b476054915ca51dbe492c0426
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2022-10-19 11:37:59 +0200

    Updated Services section

--- website-react-ts/src/Components/Services.tsx
+++ website-react-ts/src/Components/Services.tsx
@@ -6,7 +6,9 @@ function Services() {
   return (
     <InfoPaper title={"Services"} subtitle="What work can I do?" id="services">
       <p>Coming soon!</p>
-      <LoremIpsum />
+      <p>
+        I offer various development services from FullStack Web and App development to custom software.
+      </p>
     </InfoPaper>
   );
 }
