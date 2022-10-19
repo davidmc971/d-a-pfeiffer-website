215f9bdaf170c3a561824bda320deb2058592dc4
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2022-10-19 11:30:26 +0200

    Automatic age

--- website-react-ts/src/Components/About.tsx
+++ website-react-ts/src/Components/About.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import InfoPaper from "./InfoPaper";
 
 function About() {
+  const birthDate = new Date("1997-12-01");
+  const age = Math.floor((Date.now() - new Date(birthDate).getTime()) / 3.15576e+10)
   return (
     <InfoPaper title="About" subtitle="Who am I?" id="about">
       <p>Welcome to my website! My name is David Alexander Pfeiffer.</p>
-      <p>I am from Aachen, Germany and am 23 years old.</p>
+      <p>I am from Aachen, Germany and am {age} years old.</p>
       <p>
         I am working as a self-employed freelancer in my business "David
         Alexander Pfeiffer Software & IT", which I started in September 2021.
